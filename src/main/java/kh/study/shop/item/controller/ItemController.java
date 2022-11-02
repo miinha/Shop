6338ba3054b10ea8e021adccc0a409394d9a6c02
miinha <alsgk27@gmail.com>
2022-11-02 15:44:21 +0900

    item_list, itemController 수정-실행확인

--- src/main/java/kh/study/shop/item/controller/ItemController.java
+++ src/main/java/kh/study/shop/item/controller/ItemController.java
@@ -1,5 +1,8 @@
 package kh.study.shop.item.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kh.study.shop.item.service.ItemService;
+import kh.study.shop.item.vo.ImgVO;
 import kh.study.shop.item.vo.ItemVO;
 
 @Controller
@@ -26,19 +30,24 @@ public class ItemController {
 	//boolean loginFail : 로그인 실패시에만 데이터를 true로 받아온다.
 	@GetMapping("/list")
 	public String itemList(boolean isLoginFail, Model model
-							, @CookieValue(required = false) String itemName) { 
+							, @CookieValue(required = false) String imgName) { 
 		
 		//상품목록 조회
 		List<ItemVO> itemList = itemService.selectItemList();
-		model.addAttribute("itemList", itemService.selectItemList());
-		model.addAttribute("cookie_itemName", itemName);
+		model.addAttribute("itemList", itemList);
 		
-		for(ItemVO e : itemList) {
-			System.out.println(e);
-		}
 		//System.out.println("~~~~~~" + loginFail); //loginFail의 데이터가 넘어올때만 비밀번호 잘못 입력 -> 이 경우에만 모달창 실행시킨다.
 		//로그인 성공, 실패 여부 데이터를 html에 전달 -> 전달한 데이터를  js에서 쓰기 위해서?
 		model.addAttribute("isLoginFail", isLoginFail);
+		
+		String[] cookieArr = imgName.split(",");
+		List<String> cookieList = Arrays.asList(cookieArr);
+		
+		model.addAttribute("cookie_imgName", cookieList); 
+		
+//		for(ItemVO e : itemList) {
+//			System.out.println(e);
+//		}
 		
 		return "content/item/item_list";
 	}
@@ -46,7 +55,8 @@ public class ItemController {
 	//상품 상세 페이지
 	@GetMapping("/itemDetail")
 	public String itemDetail(Model model, String itemCode 
-							, HttpServletResponse response) {
+							, HttpServletResponse response
+							, @CookieValue(required = false, name = "imgName") String cookieImgName) {
 		ItemVO item = itemService.selectItemDetailList(itemCode);
 		model.addAttribute("itemInfo", item);
 		
@@ -66,11 +76,43 @@ public class ItemController {
 		Cookie cookie2 = new Cookie("age", "20");
 		response.addCookie(cookie2);
 		
-		//상세보기 한 상품명을 쿠키로 저장
-		Cookie cookie_itemName = new Cookie("itemName", item.getItemName());
-		response.addCookie(cookie_itemName);
+		//상세보기 한 상품의 대표 이미지를 쿠키로 저장
+		String imgName = "";
+		for(ImgVO img : item.getImgList()) {
+			if(img.getIsMain().equals("Y")) {
+				imgName = img.getAttachedName();
+			}
+		}
+		
+		//java.net
+		//인코딩만 진행하면 공백이 + 문자로 바뀐다. ex> "안 녕" -> "안+녕" : +를 공백으로 바꾸어주어야 한다.
+		String encodeImgName = null;
+		
+		if(cookieImgName != null) {
+			encodeImgName = getEncodeStr(cookieImgName + "," + imgName);
+		}
+		else {
+			encodeImgName = getEncodeStr(imgName);
+		}
+		
+		Cookie cookie_imgName = new Cookie("imgName", encodeImgName);
+		response.addCookie(cookie_imgName);
 		
 		return "content/item/item_detail";
 	}
+	
+	
+	public String getEncodeStr(String str) {
+		//java.net
+		//인코딩만 진행하면 공백이 + 문자로 바뀐다. ex> "안 녕" -> "안+녕" : +를 공백으로 바꾸어주어야 한다.
+		String result = "";
+		try {
+			result = URLEncoder.encode(str, "UTF-8").replaceAll("\\+", "%20");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
 	
 }
